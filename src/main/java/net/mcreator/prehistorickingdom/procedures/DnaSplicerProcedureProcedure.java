ff494438594e4c532a74b6b809fa1ff8d599e12c
dkmk100 <Home@192.168.0.139>
2020-06-15 20:30:14 -0700

    DNA splicer: put a chicken DNA in the very left slot and a diamond block in the middle slot to get a diamond chicken DNA

---
+++ src/main/java/net/mcreator/prehistorickingdom/procedures/DnaSplicerProcedureProcedure.java
@@ -0,0 +1,13 @@
+package net.mcreator.prehistorickingdom.procedures;
+
+import net.mcreator.prehistorickingdom.PrehistoricKingdomModElements;
+
+@PrehistoricKingdomModElements.ModElement.Tag
+public class DnaSplicerProcedureProcedure extends PrehistoricKingdomModElements.ModElement {
+	public DnaSplicerProcedureProcedure(PrehistoricKingdomModElements instance) {
+		super(instance, 30);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+	}
+}
